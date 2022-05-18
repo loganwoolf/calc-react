989ee4b642b1c5879afaa841b91a12f1cd142aa0
Logan Woolf <loganwoolf@gmail.com>
2022-05-18 17:12:40 -0600

    Move state and click handler to useButtons hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,90 +1,10 @@
-import { useState } from "react";
 import "./App.css";
 import Buttons from "./components/Buttons";
 import Screen from "./components/Screen";
-import calculate from "./lib/calculate";
+import useButtons from "./hooks/useButtons";
 
 function App() {
-  const [display, setDisplay] = useState({ primary: "0", secondary: "" });
-  const [operands, setOperands] = useState([]);
-  const [operator, setOperator] = useState(null);
-
-  const DISPLAYLENGTH = 12;
-
-  const runCalc = (a, b) =>
-    calculate[operator](a, b)
-      .toPrecision(DISPLAYLENGTH)
-      .replace(/\.?0+$/, "");
-
-  const handleClick = (e) => {
-    const input = e.target.textContent;
-    const inputType = e.target.className;
-
-    if (inputType.includes("clear")) {
-      if (display.primary !== "0") {
-        return setDisplay((prev) => ({ ...prev, primary: "0" }));
-      }
-      setDisplay({ primary: "0", secondary: "" });
-      setOperands([]);
-      setOperator(null);
-      return;
-    }
-
-    if (inputType.includes("number")) {
-      return setDisplay((prev) => {
-        if (prev.primary === "0") {
-          return { ...prev, primary: input };
-        }
-        if (prev.primary === "-0") {
-          return { ...prev, primary: `-${input}` };
-        }
-        const update = prev.primary + input;
-        return { ...prev, primary: update };
-      });
-    }
-
-    if (inputType.includes("decimal")) {
-      if (display.primary.includes(".")) {
-        return;
-      }
-      return setDisplay((prev) => {
-        const update = prev.primary + input;
-        return { ...prev, primary: update };
-      });
-    }
-
-    if (inputType.includes("negative")) {
-      return setDisplay((prev) => {
-        if (prev.primary.includes("-")) {
-          const update = prev.primary.slice(1);
-          return { ...prev, primary: update };
-        }
-        const update = `-${prev.primary}`;
-        return { ...prev, primary: update };
-      });
-    }
-
-    if (inputType.includes("operator")) {
-      if (operands.length === 0) {
-        setOperands([display.primary]);
-        setOperator(input);
-        setDisplay({ primary: "0", secondary: `${display.primary} ${input}` });
-        return;
-      }
-    }
-
-    if (inputType.includes("equals")) {
-      if (operands.length === 1) {
-        return setOperands((prev) => {
-          console.log(prev);
-          const result = runCalc(...prev, display.primary);
-          setOperator(null);
-          setDisplay({ primary: result, secondary: "" });
-          return [];
-        });
-      }
-    }
-  };
+  const { handleClick, display } = useButtons();
 
   return (
     <div className="App">
---
+++ src/hooks/useButtons.js
@@ -0,0 +1,114 @@
+import { useState } from "react";
+import calculate from "../lib/calculate";
+
+export default function useButtons() {
+  const [display, setDisplay] = useState({ primary: "0", secondary: "" });
+  const [operands, setOperands] = useState([]);
+  const [operator, setOperator] = useState(null);
+
+  const DISPLAYLENGTH = 12;
+
+  const runCalc = (a, b) =>
+    calculate[operator](a, b)
+      .toPrecision(DISPLAYLENGTH)
+      .replace(/\.?0+$/, "");
+
+  const handleClear = () => {
+    if (display.primary !== "0") {
+      return setDisplay((prev) => ({ ...prev, primary: "0" }));
+    }
+    setDisplay({ primary: "0", secondary: "" });
+    setOperands([]);
+    setOperator(null);
+    return;
+  };
+
+  const handleNumber = (e) => {
+    const input = e.target.textContent;
+    return setDisplay((prev) => {
+      if (prev.primary === "0") {
+        return { ...prev, primary: input };
+      }
+      if (prev.primary === "-0") {
+        return { ...prev, primary: `-${input}` };
+      }
+      const update = prev.primary + input;
+      return { ...prev, primary: update };
+    });
+  };
+
+  const handleDecimal = () => {
+    if (display.primary.includes(".")) {
+      return;
+    }
+    return setDisplay((prev) => {
+      const update = prev.primary + ".";
+      return { ...prev, primary: update };
+    });
+  };
+
+  const handleNegative = () => {
+    return setDisplay((prev) => {
+      if (prev.primary.includes("-")) {
+        const update = prev.primary.slice(1);
+        return { ...prev, primary: update };
+      }
+      const update = `-${prev.primary}`;
+      return { ...prev, primary: update };
+    });
+  };
+
+  const handleOperator = (e) => {
+    const input = e.target.textContent;
+    if (operands.length === 0) {
+      setOperands([display.primary]);
+      setOperator(input);
+      setDisplay({ primary: "0", secondary: `${display.primary} ${input}` });
+      return;
+    }
+  };
+
+  const handleEquals = () => {
+    if (operands.length === 1) {
+      return setOperands((prev) => {
+        const result = runCalc(...prev, display.primary);
+        setOperator(null);
+        setDisplay({ primary: result, secondary: "" });
+        return [];
+      });
+    }
+  };
+
+  const handleClick = (e) => {
+    const inputType = e.target.className;
+
+    if (inputType.includes("clear")) {
+      handleClear();
+    }
+
+    if (inputType.includes("number")) {
+      handleNumber(e);
+    }
+
+    if (inputType.includes("decimal")) {
+      handleDecimal();
+    }
+
+    if (inputType.includes("negative")) {
+      handleNegative();
+    }
+
+    if (inputType.includes("operator")) {
+      handleOperator(e);
+    }
+
+    if (inputType.includes("equals")) {
+      handleEquals();
+    }
+  };
+
+  return {
+    display,
+    handleClick,
+  };
+}
