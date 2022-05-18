40110eecdf50959360a333656b6eba5f8dd0eac5
Logan Woolf <loganwoolf@gmail.com>
2022-05-18 12:03:14 -0600

    Add negative button and modify layout of buttons

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,25 +1,28 @@
 import "./Buttons.css";
 
 export default function Buttons(props) {
+
+
   return (
-    <div className="input-buttons">
-      <button className="operator-button">{"C"}</button>
+    <div onClick={props.onClick} className="input-buttons">
+      <button className="clear-button">{"C"}</button>
+      <button className="negative-button">{"+/-"}</button>
       <button className="operator-button">{"/"}</button>
       <button className="operator-button">{"*"}</button>
-      <button className="operator-button">{"-"}</button>
       <button className="number-button">{"7"}</button>
       <button className="number-button">{"8"}</button>
       <button className="number-button">{"9"}</button>
-      <button className="operator-button tall">{"+"}</button>
+      <button className="operator-button">{"-"}</button>
       <button className="number-button">{"4"}</button>
       <button className="number-button">{"5"}</button>
       <button className="number-button">{"6"}</button>
+      <button className="operator-button">{"+"}</button>
       <button className="number-button">{"1"}</button>
       <button className="number-button">{"2"}</button>
       <button className="number-button">{"3"}</button>
-      <button className="operator-button tall">{"="}</button>
+      <button className="equals-button tall">{"="}</button>
       <button className="number-button wide">{"0"}</button>
-      <button className="number-button">{"."}</button>
+      <button className="decimal-button">{"."}</button>
     </div>
   );
 }
