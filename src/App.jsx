424194190e9e56fb3187d40445eba91968c4512a
Logan Woolf <loganwoolf@gmail.com>
2022-05-18 12:04:59 -0600

    Add Screen to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
+import { useState } from "react";
 import "./App.css";
 import Buttons from "./components/Buttons";
+import Screen from "./components/Screen";
 
 function App() {
+  const [display, setDisplay] = useState({ primary: "0", secondary: "" });
+  const [operands, setOperands] = useState([null, null])
+  
+  const handleClick = (e) => {
+  };
+
   return (
     <div className="App">
-      <Buttons />
+      <Screen display={display} />
+      <Buttons onClick={handleClick} />
     </div>
   );
 }
