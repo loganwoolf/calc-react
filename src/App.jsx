ce03711ecbab07ebef18db1e204531118ca144b2
Logan Woolf <loganwoolf@gmail.com>
2022-05-19 19:59:11 -0600

    Add attribution footer

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import useButtons from "./hooks/useButtons";
 
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
+import Footer from "./components/Footer";
 // import the next line into each file fontawesome is needed
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 library.add(fas);
@@ -13,10 +14,13 @@ function App() {
   const { handleClick, display } = useButtons();
 
   return (
-    <div className="App">
-      <Screen display={display} />
-      <Buttons onClick={handleClick} />
-    </div>
+    <>
+      <div className="App">
+        <Screen display={display} />
+        <Buttons onClick={handleClick} />
+      </div>
+      <Footer />
+    </>
   );
 }
 
---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer>
+      <p>
+        Made in 🇨🇦 by <span>Logan Woolf</span>
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
